434ee05613772af4901ef686f42344f1d6381975
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-18 20:42:39 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here: 
   epic_tragedy[:Montague][:hero][:status] = "dead"
-  :Capulet[:heroine][:status] = "dead"
+  epic_tragedy[:Capulet][:heroine][:status] = "dead"
 
   
 
